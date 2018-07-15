2743cab4435dc1cb02d22c856fa6612f3a953be8
bunnymarie <bunnymarie@protonmail.com>
2018-07-15 12:55:35 -0700

    Controller finished, ready for first test.

--- Controllers/RepeatCountController.cs
+++ Controllers/RepeatCountController.cs
@@ -2,3 +2,22 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using WordSearch.Models;
+
+namespace WordSearch.Controllers
+{
+  public class RepeatCountController : Controllers
+  {
+    [HttpGet("/form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+    [HttpPost("/form/result")]
+    public ActionResult RepeatCount()
+    {
+      RepeatCounter newUserWriting = new RepeatCounter(Request.Form["user-writing"]);
+      RepeatCounter newUserWord = new RepeatCounter(Request.Form["user-word"]);
+      return View(model);
+    }
+  }
+}
